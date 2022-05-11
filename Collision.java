e5be4aeb5b504a589ff31169a65ab6915baa637d
rorokapur <kapurkrohan@outlook.com>
2022-05-11 14:00:27 -0700

    added: collision class

---
+++ Collision.java
@@ -0,0 +1,27 @@
+public class Collision {
+   // Enums for different types of collisions
+   public enum Side {
+      LEFT,
+      RIGHT,
+      TOP,
+      BOTTOM
+   }
+   public enum Type {
+      NORMAL,
+      DEADLY,
+      SLIPPERY
+   }
+   
+   // Properties of collision
+   private Type collisionType;
+   private Side collisionSide;
+   
+   public Collision(Side side, Type type) {
+      collisionSide = side;
+      collisionType = type;
+   }
+   
+   // Getters
+   public Side getSide() {return collisionSide;}
+   public Type getType() {return collisionType;}
+}
