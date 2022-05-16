c04424557ed14eccc42686b0448f71bf0ac285ee
Kazimour <1cpkazimour@seattleschools.org>
2022-05-16 15:22:52 -0700

    Removed test printlns

--- LevelElement.java
+++ LevelElement.java
@@ -65,14 +65,12 @@ public class LevelElement implements Serializable {
       
       // Left gap is the smallest
       if (left_gap > right_gap && left_gap > top_gap && left_gap > bottom_gap) {
-         System.out.println("L" + left_gap);
          lit_right = true;
          return new Collision(Collision.Side.LEFT, Collision.Type.NORMAL, x2, -left_gap);
       }
       
       // We don't need to check the left gap anymore
       if (right_gap > top_gap && right_gap > bottom_gap) {
-         System.out.println("R" + right_gap);
          lit_left = true;
          return new Collision(Collision.Side.RIGHT, Collision.Type.NORMAL, x1 - size, -right_gap);
       }
