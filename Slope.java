35bb2d900de42a2999fa70cdb80fc18158d23ecb
rorokapur <kapurkrohan@gmail.com>
2022-05-18 14:08:37 -0700

    added framework for slope collisions

--- Slope.java
+++ Slope.java
@@ -1,9 +1,18 @@
 import java.lang.Math;
 
 public class Slope extends LevelElement {
+
+   // Direction of slope
+   private Direction direction;
+
+   enum Direction {
+      LEFT,
+      RIGHT
+   }
    
-   public Slope(int xone, int xtwo, int yone, int ytwo) {
+   public Slope(int xone, int xtwo, int yone, int ytwo, Direction d) {
       super(xone, xtwo, yone, ytwo);
+      direction = d;
    }
    
    // Returns downward angle of top face in degrees
@@ -11,6 +20,22 @@ public class Slope extends LevelElement {
       int a = super.getY2() - super.getY1();
       int b = super.getX2() - super.getX1();
       return Math.atan((double) a / (double) b) * (180 / Math.PI);
+   }
+   
+   // Checks collisions with player
+   public Collision checkCollisions(int x, int y) {
+      Collision box = super.checkCollisions(x, y);
+      // Checks if the player collides with the sides or top of the slope
+      if (box != null) {
+         if (box.getSide() == Collision.Side.RIGHT && direction == Direction.LEFT) {
+         }
+         if (box.getSide() == Collision.Side.LEFT && direction == Direction.RIGHT) {
+         }
+         if (box.getSide() == Collision.Side.BOTTOM) {
+         }
+         
+      }
+      return box;
    }
    
    // Code for testing
