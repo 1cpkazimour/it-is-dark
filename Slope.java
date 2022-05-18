395da5301fb350d023a9f284652403e736c4222e
rorokapur <kapurkrohan@gmail.com>
2022-05-17 21:54:05 -0700

    added getSlope method and some testing code

--- Slope.java
+++ Slope.java
@@ -6,9 +6,18 @@ public class Slope extends LevelElement {
       super(xone, xtwo, yone, ytwo);
    }
    
+   // Returns downward angle of top face in degrees
    public double getSlope() {
       int a = super.getY2() - super.getY1();
       int b = super.getX2() - super.getX1();
-      return Math.atan((double) a / (double) b);
+      return Math.atan((double) a / (double) b) * (180 / Math.PI);
    }
+   
+   // Code for testing
+   //public static void main(String[] args) {
+   //   Slope test = new Slope(1, 1, 2, 2);
+   //   System.out.println(test.getSlope());
+   //}
+   
+   
 }
