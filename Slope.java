703d1bb9d5d203d8ae45f11c4326e2d96210d976
Kazimour <1cpkazimour@seattleschools.org>
2022-06-06 15:35:30 -0700

    I'm lost

--- Slope.java
+++ Slope.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+import java.awt.*;
 
 public class Slope extends LevelElement {
 
@@ -29,7 +30,7 @@ public class Slope extends LevelElement {
       if (box == null) return null;
    
       //System.out.println("x: " + x + " y: " + y + " d: " + d + " refx: " + refx + " refy: " + refy);
-      if ((box.getSide() == Collision.Side.RIGHT || box.getSide() == Collision.Side.BOTTOM) && direction == Direction.RIGHT) {
+      if ((box.getSide() == Collision.Side.LEFT || box.getSide() == Collision.Side.BOTTOM) && direction == Direction.RIGHT) {
          y += Player.SIZE;
          double xa = super.getX1();
          double xb = super.getX2();
@@ -40,7 +41,7 @@ public class Slope extends LevelElement {
          double refy = c * (refx - xa) + y;
          double d = Math.sqrt(Math.pow((refx - x), 2) + Math.pow((refy - y), 2));
          if (y > refy) {
-            return new SlopeCollision(Collision.Side.BOTTOM, Collision.Type.NORMAL, (int) refx, (int) refy - Player.SIZE, (int) d);         
+            return new SlopeCollision(Collision.Side.BOTTOM, Collision.Type.NORMAL, (int) refx, (int) refy - Player.SIZE, 0);         
          } else {
             return null;
          }
@@ -54,14 +55,23 @@ public class Slope extends LevelElement {
          double c = (yb - ya) / (xb - xa);
          double refx = -((c * xa) + ya + (x/c) - y) / (-(1 / c) - c);
          double refy = c * (refx - xa) + y;
-         double d = Math.sqrt(Math.pow((refx - x), 2) + Math.pow((refy - y), 2));
+         double d = Math.sqrt(Math.pow(refx - x, 2) + Math.pow(refy - y, 2));
          if (y > refy) {
-            return new SlopeCollision(Collision.Side.BOTTOM, Collision.Type.NORMAL, (int) refx - Player.SIZE, (int) refy - Player.SIZE, (int) d);
+            return new SlopeCollision(Collision.Side.BOTTOM, Collision.Type.NORMAL, (int) refx - Player.SIZE, (int) refy - Player.SIZE, 0);
          } else {
             return null;
          }
       } else {
          return box;
+      }
+   }
+   
+   public void drawFaces(Graphics g) {
+      super.drawFaces(g);
+      if (direction == Direction.LEFT) {
+         g.drawLine(super.getX1(), super.getY2(), super.getX2(), super.getY1());
+      } else {
+         g.drawLine(super.getX1(), super.getY1(), super.getX2(), super.getY2());
       }
    }
    
