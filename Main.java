77bec0a78768da3f40bfc8afbd10fe7d273fb924
SereneMonk <95724771+SereneMonk@users.noreply.github.com>
2022-06-15 20:51:17 -0700

    More levels
    Levels 16-24 (I think)

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
-* It is dark is a simple platformer game with basic graphics and unique gameplay.
-* In each level, the player starts with a black screen, and has to slowly reveal the level
-* by moving around to reveal the surfaces in the level.
-* @author Cameron Kazimour, Julian Shah, Rohan Kapur
-*/
+ * It is dark is a simple platformer game with basic graphics and unique
+ * gameplay.
+ * In each level, the player starts with a black screen, and has to slowly
+ * reveal the level
+ * by moving around to reveal the surfaces in the level.
+ * 
+ * @author Cameron Kazimour, Julian Shah, Rohan Kapur
+ */
 
 public class Main extends JFrame {
    // Go into edit mode?
