af4c8f2ce1628a0c4ea2e9a7b8e66a276164221f
Kazimour <1cpkazimour@seattleschools.org>
2022-06-16 18:42:16 -0700

    Added proper lighting to slopes

--- Slope.java
+++ Slope.java
@@ -5,12 +5,21 @@ import java.awt.*;
  * A type of LevelElement that implements a sloped surface
  */
 public class Slope extends LevelElement {
+   /**
+    * Internal UID for serialization.
+    */
+    private static final long serialVersionUID = -4083507241221796397L;
 
    /**
     * Direction of the slope
     */
    private Direction direction;
    
+   /**
+    * Whether or not the slant face has been illumnated (collided with)
+    */
+   private boolean lit_slant;
+
    /**
     * Color of the slope (white)
     */
@@ -37,6 +46,7 @@ public class Slope extends LevelElement {
    public Slope(int xone, int yone, int xtwo, int ytwo, Direction d) {
       super(xone, yone, xtwo, ytwo);
       direction = d;
+      lit_slant = false;
    }
 
    /**
@@ -66,6 +76,11 @@ public class Slope extends LevelElement {
     * @return A Collision object containing collision data if applicable, otherwise null.
     */
    public Collision checkCollisions(int x, int y) {
+      // Store these temporary values, in case we need them if the boxCollision doesn't apply
+      boolean lit_bottom = super.lit_bottom;
+      boolean lit_left = super.lit_left;
+      boolean lit_right = super.lit_right;
+
       Collision boxCollision = super.checkCollisions(x, y);
       // Checks if the player collides with the sides or top of the slope
       if (boxCollision == null) 
@@ -113,10 +128,26 @@ public class Slope extends LevelElement {
       if ((boxCollision.getSide() == Collision.Side.BOTTOM) ||
           (boxCollision.getSide() == Collision.Side.LEFT && direction == Direction.RIGHT) ||
           (boxCollision.getSide() == Collision.Side.RIGHT && direction == Direction.LEFT)) {
+         // Restore pre-boxCollision values
+         super.lit_right = lit_right;
+         super.lit_left = lit_left;
+         super.lit_bottom = lit_bottom;
+         // We've collided with a slope if slopeCollision isn't null
+         lit_slant |= slopeCollision != null;
          return slopeCollision;
       } else if (slopeCollision == null) {
+         // Restore pre-boxCollision values
+         super.lit_right = lit_right;
+         super.lit_left = lit_left;
+         super.lit_bottom = lit_bottom;
          return null;
       } else if (slopeCollision.getDegree() < boxCollision.getDegree()) {
+         // Restore pre-boxCollision values
+         super.lit_right = lit_right;
+         super.lit_left = lit_left;
+         super.lit_bottom = lit_bottom;
+         // We've collided with a slope (slopeCollision isn't null here)
+         lit_slant = true;
          return slopeCollision;
       } else {
          return boxCollision;
@@ -139,12 +170,14 @@ public class Slope extends LevelElement {
          g.fillRect(super.getX2(), super.getY1(), 2, super.getY2() - super.getY1());
       }
       
-      if (direction == Direction.LEFT) {
-         g.drawLine(super.getX1(), super.getY2() - 2, super.getX2(), super.getY1());
-         g.drawLine(super.getX1(), super.getY2() - 1, super.getX2(), super.getY1() + 1);
-      } else {
-         g.drawLine(super.getX1(), super.getY1() - 2, super.getX2(), super.getY2());
-         g.drawLine(super.getX1(), super.getY1() - 1, super.getX2(), super.getY2() + 1);
+      if (lit_slant) {
+         if (direction == Direction.LEFT) {
+            g.drawLine(super.getX1(), super.getY2() - 2, super.getX2(), super.getY1());
+            g.drawLine(super.getX1(), super.getY2() - 1, super.getX2(), super.getY1() + 1);
+         } else {
+            g.drawLine(super.getX1(), super.getY1() - 2, super.getX2(), super.getY2());
+            g.drawLine(super.getX1(), super.getY1() - 1, super.getX2(), super.getY2() + 1);
+         }
       }
    }
 }
